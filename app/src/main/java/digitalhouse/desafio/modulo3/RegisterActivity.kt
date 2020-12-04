1dc9bf10e444d8c5475f7b56031d87c7744165b9
Renan Silva <renanh2008@hotmail.com>
2020-12-03 23:50:00 -0300

    Criando tela de registro

---
+++ app/src/main/java/digitalhouse/desafio/modulo3/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package digitalhouse.desafio.modulo3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
