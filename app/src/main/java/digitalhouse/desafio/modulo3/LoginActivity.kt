d2f9ea074910c93bb8f1e2c88d2dab756de81ab1
Renan Silva <renanh2008@hotmail.com>
2020-12-04 01:19:54 -0300

    Removendo a toolbar e ajustes

--- app/src/main/java/digitalhouse/desafio/modulo3/LoginActivity.kt
+++ app/src/main/java/digitalhouse/desafio/modulo3/LoginActivity.kt
@@ -1,11 +1,25 @@
 package digitalhouse.desafio.modulo3
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btn_login).setOnClickListener(openActivity(HomeActivity::class.java))
+        findViewById<TextView>(R.id.tx_createAccout).setOnClickListener(openActivity(RegisterActivity::class.java))
+    }
+
+    fun openActivity(activity: Class<out Activity>): View.OnClickListener? {
+        return View.OnClickListener {
+            startActivity(Intent(this, activity))
+        }
     }
 }
